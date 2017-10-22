79db5a3105d2f81332f89478739f228e5dd11945
Trevor Sharp <sharp.trevor@me.com>
2017-10-22 12:55:51 -0700

    Rename kidneyData to backend

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from flask_compress import Compress
-import csv, kidneyData, json
+import csv, backend, json
 
 app = Flask(__name__)
 Compress(app)
@@ -34,24 +34,24 @@ def financial():
 
 #@app.route('/myFormSubmit', methods=['POST'])
 #def myFormSubmit():
-#    kidneyData.start(json.dumps(request.form), 4) #this number is passed in and links up with the python script that is run to display the correct form type in the email.
+#    backend.start(json.dumps(request.form), 4) #this number is passed in and links up with the python script that is run to display the correct form type in the email.
 #   return render_template('response.html')
 
 #************End of Add New Form******************
 
 @app.route('/medicationsFormSubmit', methods=['POST'])
 def medicationsFormSubmit():
-    kidneyData.start(json.dumps(request.form), 1)
+    backend.start(json.dumps(request.form), 1)
     return render_template('response.html')
 
 @app.route('/transportationFormSubmit', methods=['POST'])
 def transportationFormSubmit():
-    kidneyData.start(json.dumps(request.form), 2)
+    backend.start(json.dumps(request.form), 2)
     return render_template('response.html')
 
 @app.route('/financialFormSubmit', methods=['POST'])
 def financialFormSubmit():
-    kidneyData.start(json.dumps(request.form), 3)
+    backend.start(json.dumps(request.form), 3)
     return render_template('response.html')
 
 @app.route('/returnHome', methods=['POST'])
