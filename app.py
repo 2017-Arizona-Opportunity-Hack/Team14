fbb4c1053187e399986c6a7c523f7294dd30a730
Bailey Garner <bwgarne1@gmail.com>
2017-10-21 22:34:06 -0700

    no message

--- app.py
+++ app.py
@@ -14,11 +14,10 @@ def transportation():
 def signUp():
     return render_template('medications-application-form.html')
 
-<<<<<<< HEAD
 @app.route("/medication-application", methods=['POST'])
 def find():
     print(cool);
-=======
+
 @app.route("/english-financial")
 def financial():
     return render_template('financial-form.html');
@@ -28,7 +27,6 @@ def add_numbers():
     a = request.args.get('a', 0, type=int)
     b = request.args.get('b', 0, type=int)
     return jsonify(result=a + b)
->>>>>>> f12f712960e592dcad52ccad05588dd2c5a0b868
 
 if __name__ == '__main__':
     app.run()
