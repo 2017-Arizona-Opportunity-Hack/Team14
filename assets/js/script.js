499aec4e565ec087f89759ed702f0740f575590c
Trevor Sharp <sharp.trevor@me.com>
2017-10-21 20:00:08 -0700

    New addMedication functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,73 @@
-var medications = [];
+var index = 0;
 
 function addMedication() {
     
     var medication = {
-        name: document.getElementById("medication-name"),
-        dosage: document.getElementById("medication-dosage"),
-        quantity: document.getElementById("medication-quantity"),
-        physician: document.getElementById("medication-physician"),
-        month: document.getElementById("medication-dateapplied-month"),
-        day: document.getElementById("medication-dateapplied-day"),
-        year: document.getElementById("medication-dateapplied-year"),
-        refills: document.getElementById("medication-refills"),
-        status: document.getElementById("medication-status"),
-        covered: document.getElementById("medication-covered")
+        name: document.getElementById("medication-name").value,
+        dosage: document.getElementById("medication-dosage").value,
+        quantity: document.getElementById("medication-quantity").value,
+        physician: document.getElementById("medication-physician").value,
+        month: document.getElementById("medication-dateapplied-month").value,
+        day: document.getElementById("medication-dateapplied-day").value,
+        year: document.getElementById("medication-dateapplied-year").value,
+        refills: "",
+        status: "",
+        covered: ""
     }
     
-    medications.push(medication);
-    
-    output = "<h4 style=\"text-align: center\">";
-    
-    if (medications.length == 1) {
-        output += "1 Medication Added!</h4>";
-    } else {
-        output += medications.length + " Medications Added!</h4>";
+    if (document.getElementById("medication-refills-yes").checked == true) {
+        medication.refills = "yes";
+    } else if (document.getElementById("medication-refills-no").checked == true) {
+        medication.refills = "no";
     }
     
-    document.getElementById("medications").innerHTML = output;
+    if (document.getElementById("medication-status-pending").checked == true) {
+        medication.status = "pending";
+    } else if (document.getElementById("medication-status-accepted").checked == true) {
+        medication.status = "accepted";
+    } else if (document.getElementById("medication-status-denied").checked == true) {
+        medication.status = "denied";
+    }
+    
+    if (document.getElementById("medication-covered-yes").checked == true) {
+        medication.covered = "yes";
+    } else if (document.getElementById("medication-covered-no").checked == true) {
+        medication.covered = "no";
+    }
+    
+    index++;
+    
+    var output = document.getElementById("medication-entry").innerHTML.toString();
+    var replace = "medication" + index + "\-";
+    output = output.replace(/medication\-/g , replace);
+    
+    document.getElementById(("medication" + index)).innerHTML = output;
+    
+    // Refill input
+    document.getElementById(replace + "name").value = medication.name;
+    document.getElementById(replace + "dosage").value = medication.dosage;
+    document.getElementById(replace + "quantity").value = medication.quantity;
+    document.getElementById(replace + "physician").value = medication.physician;
+    document.getElementById(replace + "dateapplied-month").value = medication.month;
+    document.getElementById(replace + "dateapplied-day").value = medication.day;
+    document.getElementById(replace + "dateapplied-year").value = medication.year;
+    if (medication.refills == "yes") {
+        document.getElementById(replace + "refills-yes").checked = true;
+    } else if (medication.refills == "no"){
+        document.getElementById(replace + "refills-no").checked = true;
+    }
+    if (medication.status == "pending") {
+        document.getElementById(replace + "status-pending").checked = true;
+    } else if (medication.status == "accepted") {
+        document.getElementById(replace + "status-accepted").checked = true;
+    } else if (medication.status == "denied") {
+        document.getElementById(replace + "status-denied").checked = true;
+    }
+    if (medication.covered == "yes") {
+        document.getElementById(replace + "covered-yes").checked = true;
+    } else if (medication.covered == "no") {
+        document.getElementById(replace + "covered-no").checked = true;
+    }
     
     // Reset Input
     document.getElementById("medication-name").value = "";
