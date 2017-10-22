5b8a19e1968b23c98ff01003396e0da1932b0465
Trevor Sharp <sharp.trevor@me.com>
2017-10-22 01:50:44 -0700

    Clean-up

--- app.py
+++ app.py
@@ -2,30 +2,21 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('form-selection.html')
+	return render_template('form-selection.html')
 
-@app.route("/transportation")
+@app.route('/transportation')
 def transportation():
-    alert("I AM IN FLASH")
-    return render_template('transportation-form.html');
+	return render_template('transportation-form.html')
   
-@app.route('/medication-application')
-def signUp():
-<<<<<<< HEAD
-    return render_template('medications-application-form.html')
+@app.route('/medications')
+def medications():
+    return render_template('medications-form.html')
 
-@app.route('/medication-application-analysis', methods = ['POST'])
-def cool():
-    return render_template('FormSelection.html');
-=======
-	return render_template('medications-form.html')
->>>>>>> a4fd2f592453dfb350972eb03c8a4b7dae9847a1
-
-@app.route("/english-financial")
+@app.route('/financial')
 def financial():
-    return render_template('financial-form.html');
+	return render_template('financial-form.html')
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
