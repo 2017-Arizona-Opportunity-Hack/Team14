f12f712960e592dcad52ccad05588dd2c5a0b868
Bailey Garner <bwgarne1@gmail.com>
2017-10-21 22:28:54 -0700

    Navigation

--- app.py
+++ app.py
@@ -5,10 +5,18 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('FormSelection.html')
+
+@app.route("/transportation")
+def transportation():
+    return render_template('transportation-form.html')
   
 @app.route("/medication-application")
 def signUp():
     return render_template('medications-application-form.html')
+
+@app.route("/english-financial")
+def financial():
+    return render_template('financial-form.html');
 
 @app.route('/_add_numbers')
 def add_numbers():
